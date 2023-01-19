75fc229a3ea19761a5d9e157ed864790cbefc2c8
hojin <905018@naver.com>
2023-01-19 23:51:43 +0900

    dp

---
+++ 알고리즘/dp/NO2225.java
@@ -0,0 +1,32 @@
+package dp;
+import java.util.Scanner;
+
+public class NO2225 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int N = s.nextInt();
+		int K = s.nextInt();
+		
+		int dp[][] = new int[N+1][K+1];
+		
+        //K=1일때 초기화
+		for(int i=1;i<=N;i++) {
+			dp[i][1]=1;
+		}
+		
+        //N=1일때 초기화
+		for(int i=1;i<=K;i++) {
+			dp[1][i]=i;
+		}
+		
+		
+		for(int i=2;i<=N;i++) {
+			for(int j=2;j<=K;j++) {
+				dp[i][j]=(dp[i-1][j]+dp[i][j-1])%1000000000;
+			}
+		}
+
+		System.out.println(dp[N][K]);
+	}
+}
